6983cc8780116db181696b66497d1424bb12a0d2
Nathan Hiemstra <mail@nathanhiemstra.com>
2022-07-29 09:23:45 -0500

    Preventing error when WP unexpectedly stops giving logo API info

--- src/components/site-logo.js
+++ src/components/site-logo.js
@@ -13,15 +13,16 @@ const SiteLogo = ({ data }) => {
     `);
 
     const logo = wpMediaItem;
-
-    return (
-        <Image
-            src={logo.sourceUrl}
-            alt={logo.altText}
-            fluid={true}
-            style={{ maxHeight: "50px" }}
-        />
-    );
+    if (logo) {
+        return (
+            <Image
+                src={logo.sourceUrl}
+                alt={logo.altText}
+                fluid={true}
+                style={{ maxHeight: "50px" }}
+            />
+        );
+    }
 };
 
 export default SiteLogo;
