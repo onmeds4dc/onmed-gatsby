610ea1453eaadb2161a5166bc5e8a72d63c9141b
Nathan Hiemstra <mail@nathanhiemstra.com>
2022-08-09 16:41:27 -0500

    Primary menu - styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,11 +6,13 @@ import MenuHeader from "./menu-header";
 
 const Header = () => {
     return (
-        <header className="container py-4 d-flex justify-content-between">
-            <Link to="/" className="text-light d-flex">
-                <SiteLogo />
-            </Link>
-            <MenuHeader />
+        <header className="bg-cloud py-4">
+            <div className="container  d-flex justify-content-between">
+                <Link to="/" className="text-light d-flex">
+                    <SiteLogo />
+                </Link>
+                <MenuHeader />
+            </div>
         </header>
     );
 };
