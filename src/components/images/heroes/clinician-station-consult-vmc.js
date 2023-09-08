b0fc2ca2b90c79580df233041813641c88fb17a5
OnMed <109237642+onmeds4dc@users.noreply.github.com>
2023-09-08 16:04:59 -0400

    Update clinician-station-consult-vmc.js

--- src/components/images/heroes/clinician-station-consult-vmc.js
+++ src/components/images/heroes/clinician-station-consult-vmc.js
@@ -1,1 +1,15 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
+export function ImgHeroBoothScreenStethoscope(props) {
+    return (
+        <StaticImage
+            src="../../../../static/images/heroes/clinician-station-consult-vmc.jpg"
+            alt={props.alt}
+            className={props.className}
+            layout="fullWidth"
+            transformOptions="cover"
+            objectPosition={props.position}
+        />
+    );
+}
