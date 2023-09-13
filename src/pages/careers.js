e7aa9b9e133c9cfe41a17ff7f0fafa36eee03030
OnMed <109237642+onmeds4dc@users.noreply.github.com>
2023-09-13 14:26:01 -0400

    Update careers.js

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -10,6 +10,8 @@ import { Link } from "gatsby";
 import KeyAudienceSellingPoints from "../modules/key-audience-selling-points";
 import MetaTags from "../components/meta-tags";
 import Hero from "../components/hero";
+import ListFigureGrid from "../components/list-figure-grid";
+import ListFigureGridItem from "../components/list-figure-grid-item";
 
 const CareersPage = ({ data }) => {
     const sectionHero = (
@@ -140,12 +142,89 @@ const CareersPage = ({ data }) => {
         </section>
     );
 
-    const sectionKeyAudienceSellingPoints = (
-        <section className="bg-cloud py-5">
-            <div className="container py-4 py-md-6">
-                <KeyAudienceSellingPoints />
-            </div>
-        </section>
+    const KeyAudienceSellingPoints1 = (props) => {
+    return (
+        <>
+            <h2 className="text-center mb-5">
+                The OnMed Care Station Experience
+            </h2>
+            <ListFigureGrid hasIcons={true}>
+                <ListFigureGridItem
+                    headline="Clean"
+                    body={
+                        <p>
+                            The Care Station is carefully sanitized using UV
+                            lighting
+                        </p>
+                    }
+                    img={<img src="/icons/icon-sunset.svg" alt="Sunset icon" />}
+                />
+                <ListFigureGridItem
+                    headline="Private"
+                    body={
+                        <p>
+                            Privacy when in use with LCD glass that is fogged
+                            completely when activated
+                        </p>
+                    }
+                    img={
+                        <img
+                            src="/icons/icon-eye-disabled.svg"
+                            alt="Eyeball with slash icon"
+                        />
+                    }
+                />
+                <ListFigureGridItem
+                    headline="Accessible"
+                    body={
+                        <p>
+                            Services available to anyone, anywhere, including
+                            rural and remote areas
+                        </p>
+                    }
+                    img={<img src="/icons/icon-map.svg" alt="Map icon" />}
+                />
+                <div className="w-100"></div>
+                <div className="w-100"></div>
+                <ListFigureGridItem
+                    headline="Affordable"
+                    body={
+                        <p>
+                            Works with major insurance and only a fraction of
+                            the cost of a traditional doctor's office visit
+                        </p>
+                    }
+                    img={
+                        <img
+                            src="/icons/icon-get-money.svg"
+                            alt="Hands and dollar symbol icon"
+                        />
+                    }
+                />
+                <ListFigureGridItem
+                    headline="Secure"
+                    body={
+                        <p>
+                            HIPAA compliant transfer and storage of all patient
+                            data using advanced encryption technology
+                        </p>
+                    }
+                    img={<img src="/icons/icon-lock.svg" alt="Lock icon" />}
+                />
+                <ListFigureGridItem
+                    headline="Convenient"
+                    body={
+                        <p>
+                            Open 7 days a week with extended hours and no
+                            appointment needed
+                        </p>
+                    }
+                    img={<img src="/icons/icon-clock.svg" alt="Clock icon" />}
+                />
+            </ListFigureGrid>
+        </>
+    );
+};
     );
 
     return (
@@ -156,7 +235,7 @@ const CareersPage = ({ data }) => {
             {sectionCommitment}
             {sectionApplyNow}
             {sectionBenefits}
-            {sectionKeyAudienceSellingPoints}
+            {sectionKeyAudienceSellingPoints1}
         </Layout>
     );
 };
