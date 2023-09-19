99edb70627c214f7331b3250b24d53d6b1257772
OnMed <109237642+onmeds4dc@users.noreply.github.com>
2023-09-19 14:41:23 -0400

    Update careers-points.js

--- src/modules/careers-points.js
+++ src/modules/careers-points.js
@@ -4,6 +4,46 @@ import ListFigureGridItem from "../components/list-figure-grid-item";
 import SvgNewWindow from "../components/svgs/new-window";
 import { Link } from "gatsby";
 import { ImgHexWalkIn } from "../components/images/hex-walk-in";
+
+import { Modal } from '../components/Modal';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`;
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(showModal => !showModal);
+  };
+
+  return (
+    <>
+      <Container>
+        <Button onClick={openModal}>I'm a modal</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+        <GlobalStyle />
+      </Container>
+    </>
+  );
+}
+
+
 const CareerPoints = (props) => {
     return (
         <>
