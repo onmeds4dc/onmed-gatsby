c0a9acac1537f43361aca2ac608fd580a29c2537
OnMed <109237642+onmeds4dc@users.noreply.github.com>
2023-09-19 13:44:35 -0400

    Update careers-points.js

--- src/modules/careers-points.js
+++ src/modules/careers-points.js
@@ -4,6 +4,11 @@ import ListFigureGridItem from "../components/list-figure-grid-item";
 import SvgNewWindow from "../components/svgs/new-window";
 import { Link } from "gatsby";
 import { ImgHexWalkIn } from "../components/images/hex-walk-in";
+import YouTube from "react-youtube";
+import Modal from "react-bootstrap/Modal";
+import MetaTags from "../components/meta-tags";
+import { VideoShortForm } from "../components/video-short-form";
+
 const CareerPoints = (props) => {
     return (
         <>
@@ -49,20 +54,9 @@ const CareerPoints = (props) => {
                     body={
                         <p>
                             Our Employees’ Well-Being is as Important as the Patients We Serve. <br /><br />
-                            <Link
-                                to="/patients/"
-                                className="btn btn-primary text-uppercase mb-3  mx-3 d-inline-flex align-items-center"
-                                target="_blank"
-                            >
-                                Learn More
-                                <SvgNewWindow
-                                    className="ms-2"
-                                    style={{
-                                        height: "14px",
-                                        width: "auto",
-                                    }}
-                                />
-                            </Link>
+                            <div className="mt-6 mt-6">
+                                <Video />
+                            </div>
                         </p>
                     }
                     img={<ImgHexWalkIn alt="An OnMed Care Station" />}
@@ -73,5 +67,44 @@ const CareerPoints = (props) => {
         </>
     );
 };
+
+function Video() {
+        const [lgShow, setLgShow] = useState(false);
+
+        return (
+            <>
+                <Button
+                    className="btn btn-primary text-uppercase mb-3 d-inline-flex align-items-center"
+                    onClick={() => setLgShow(true)}
+                >
+                    <SvgVideoCamera
+                        className="me-2
+                                "
+                        style={{
+                            height: "14px",
+                            width: "auto",
+                        }}
+                    />{" "}
+                    Watch the video
+                </Button>
+
+                <Modal
+                    size="lg"
+                    show={lgShow}
+                    onHide={() => setLgShow(false)}
+                    aria-labelledby="example-modal-sizes-title-lg"
+                    centered
+                >
+                    <Modal.Header closeButton></Modal.Header>
+                    <Modal.Body className="d-flex justify-content-center align-items-center">
+                        <YouTube
+                            videoId="u84r7ZQar4Q"
+                            className="youtube-container"
+                        />
+                    </Modal.Body>
+                </Modal>
+            </>
+        );
+    }
 
 export default CareerPoints;
