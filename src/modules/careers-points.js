9357ad01a1383eac35dbac5106039be48d99c675
OnMed <109237642+onmeds4dc@users.noreply.github.com>
2023-09-19 13:35:05 -0400

    Update careers-points.js

--- src/modules/careers-points.js
+++ src/modules/careers-points.js
@@ -52,7 +52,10 @@ const CareerPoints = (props) => {
                     body={
                         <p>
                             Our Employees’ Well-Being is as Important as the Patients We Serve. <br /><br />
-                            
+                function Video() {
+        const [lgShow, setLgShow] = useState(false);
+
+        return (
             <>
                 <Button
                     className="btn btn-primary text-uppercase mb-3 d-inline-flex align-items-center"
@@ -85,6 +88,7 @@ const CareerPoints = (props) => {
                     </Modal.Body>
                 </Modal>
             </>
+        );
                         </p>
                     }
                     img={<ImgHexWalkIn alt="An OnMed Care Station" />}
