5b30720e1c3ba99e072d5bafc93f842ed8e31c46
OnMed <109237642+onmeds4dc@users.noreply.github.com>
2023-09-21 12:09:04 -0400

    Update careers-points.js

--- src/modules/careers-points.js
+++ src/modules/careers-points.js
@@ -12,43 +12,7 @@ import Modal from "react-bootstrap/Modal";
 import { VideoShortForm } from "../components/video-short-form";
 
 
-function Video() {
-        const [lgShow, setLgShow] = useState(false);
 
-        return (
-            <>
-                <Button
-                    className="btn btn-primary text-uppercase mb-3 d-inline-flex align-items-center"
-                    onClick={() => setLgShow(true)}
-                >
-                    <SvgVideoCamera
-                        className="me-2
-                                "
-                        style={{
-                            height: "14px",
-                            width: "auto",
-                        }}
-                    />{" "}
-                    Watch the video
-                </Button>
-
-                <Modal
-                    size="lg"
-                    show={lgShow}
-                    onHide={() => setLgShow(false)}
-                    aria-labelledby="example-modal-sizes-title-lg"
-                    centered
-                >
-                    <Modal.Header closeButton></Modal.Header>
-                    <Modal.Body className="d-flex justify-content-center align-items-center">
-                        <YouTube
-                            videoId="u84r7ZQar4Q"
-                            className="youtube-container"
-                        />
-                    </Modal.Body>
-                </Modal>
-            </>
-        );
 
 const CareerPoints = (props) => {
     
@@ -123,5 +87,44 @@ const CareerPoints = (props) => {
         </>
     );
 };
+
+function Video() {
+        const [lgShow, setLgShow] = useState(false);
+
+        return (
+            <>
+                <Button
+                    className="btn btn-primary text-uppercase mb-3 d-inline-flex align-items-center"
+                    onClick={() => setLgShow(true)}
+                >
+                    <SvgVideoCamera
+                        className="me-2
+                                "
+                        style={{
+                            height: "14px",
+                            width: "auto",
+                        }}
+                    />{" "}
+                    Watch the video
+                </Button>
+
+                <Modal
+                    size="lg"
+                    show={lgShow}
+                    onHide={() => setLgShow(false)}
+                    aria-labelledby="example-modal-sizes-title-lg"
+                    centered
+                >
+                    <Modal.Header closeButton></Modal.Header>
+                    <Modal.Body className="d-flex justify-content-center align-items-center">
+                        <YouTube
+                            videoId="u84r7ZQar4Q"
+                            className="youtube-container"
+                        />
+                    </Modal.Body>
+                </Modal>
+            </>
+        );
+    }
 
 export default CareerPoints;
