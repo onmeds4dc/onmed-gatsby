9f69b9559485b19c60e34fd6db4c732bff0baaf1
OnMed <109237642+onmeds4dc@users.noreply.github.com>
2023-09-28 16:02:36 -0400

    Update benefits.js

--- src/pages/benefits.js
+++ src/pages/benefits.js
@@ -29,6 +29,8 @@ const BenefitsPage = ({ data }) => {
             <div className="text-center">
                 <h2 className="text-center mb-5">Our Benefits</h2>
             </div>
+            &nbsp;
+            &nbsp;
             <div className="row justify-content-center">
                 <div className="col-lg-10 col-xl-8">
                     <dl className="dl-primary lead row">
