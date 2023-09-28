6776a18886d184cabb232e8ad6ebb8d5efbc532e
OnMed <109237642+onmeds4dc@users.noreply.github.com>
2023-09-28 15:54:39 -0400

    Update benefits.js

--- src/pages/benefits.js
+++ src/pages/benefits.js
@@ -27,10 +27,10 @@ const BenefitsPage = ({ data }) => {
         <section className="container py-6 my-4">
         
             <div className="row justify-content-center">
-        <div className="text-center">
-                <h2 className="text-center mb-5">Our Benefits</h2>
-            </div>
                 <div className="col-lg-10 col-xl-8">
+                    <div className="text-center">
+                        <h2 className="text-center mb-5">Our Benefits</h2>
+                    </div>
                     <dl className="dl-primary lead row">
                         <dt>Health & Wellness</dt>
                         <dd>
